eb229e74973396b9c391d034c8cfb03d3f29d719
alyeavui <156894513+alyeavui@users.noreply.github.com>
2024-05-03 23:03:19 +0500

    Update phhonebook2.py

--- lab11/phhonebook2.py
+++ lab11/phhonebook2.py
@@ -1,6 +1,6 @@
 import psycopg2
 
-conn = psycopg2.connect(host="localhost", dbname="postgres", user="postgres", password="123456", port=5432)
+conn = psycopg2.connect(host="localhost", dbname="postgres", user="alyeavui", password="1234", port=5432)
 
 cur = conn.cursor()
 
